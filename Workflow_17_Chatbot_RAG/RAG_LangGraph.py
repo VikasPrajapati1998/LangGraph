0bd003099c77d749df0e5f1b45e96555f9338800
VikasPrajapati1998 <vikasprajapati191198@gmail.com>
2026-01-29 22:29:41 +0530

    Updated langgraph

--- Workflow_17_Chatbot_RAG/RAG_LangGraph.py
+++ Workflow_17_Chatbot_RAG/RAG_LangGraph.py
@@ -1,4 +1,4 @@
-# from typing import TypedDict, Annotated, List
+from typing import TypedDict, Annotated, List
 
 from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
@@ -6,17 +6,15 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 
-# from langchain_core.tools import tool
-# from langchain_core.messages import HumanMessage, BaseMessage
+from langchain_core.tools import tool
+from langchain_core.messages import HumanMessage, BaseMessage
 
-# from langgraph.graph import StateGraph, START, END
-# from langgraph.graph.message import add_messages
-# from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
 
 
-# LLM Model
-llm = ChatOllama(model="qwen2.5:0.5b", temperature=0.3)
-
+# ========== RAG ==========
 # Embedding Model
 embedding = OllamaEmbeddings(model="nomic-embed-text:v1.5")
 
@@ -33,5 +31,71 @@ vector_store = FAISS.from_documents(documents=chunks, embedding=embedding)
 
 # Retriever
 retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={'k': 4})
-result = retriever.invoke("What is a Decision Tree")
-print(result)
+# result = retriever.invoke("What is a Decision Tree. Explain in detail.")
+# print(result)
+
+# ========== Tool ==========
+@tool
+def rag_tool(query):
+    """
+    Retrieve relevant information from the pdf document.
+    Use this tool when the user asks factual / conceptual questions
+    that might be answered from the stored documents.
+    """
+    result = retriever.invoke(query)
+    context = [doc.page_content for doc in result]
+    metadata = [doc.metadata for doc in result]
+
+    return {
+        'query': query,
+        'context': context,
+        'metadata': metadata
+    }
+
+tools = [rag_tool]
+
+# LLM Model
+llm = ChatOllama(model="qwen2.5:3b", temperature=0.4)
+model = llm.bind_tools(tools)
+
+# State
+class ChatState(TypedDict):
+    messages: Annotated[List[BaseMessage], add_messages]
+
+# Node
+def chat_node(state: ChatState):
+    response = model.invoke(state["messages"])
+    return {"messages": [response]}
+
+tool_node = ToolNode(tools)
+
+# Graph
+graph = StateGraph(ChatState)
+
+graph.add_node("chat_node", chat_node)
+graph.add_node("tools", tool_node)
+
+graph.add_edge(START, "chat_node")
+graph.add_conditional_edges(
+    "chat_node",
+    tools_condition,
+    {
+        "tools": "tools",
+        END: END
+    }
+)
+graph.add_edge("tools", "chat_node")
+
+chatbot = graph.compile()
+
+# Run
+while True: 
+    print("="*80)
+    query = input("User: ")
+    if query.lower().strip() in ["exit", "quit"]:
+        break
+    response = chatbot.invoke({"messages": [HumanMessage(content=(query))]})
+    result = response['messages'][-1].content
+    print("Bot: ", result)
+    print("="*80)
+
---
+++ Workflow_17_Chatbot_RAG/test.py
@@ -0,0 +1,24 @@
+def get_chunk(text: str, chunk_size: int = 3, overlap: int = 2):
+    chunks = []
+    text_size = len(text)
+
+    base_size = text_size // chunk_size
+
+    for i in range(chunk_size):
+        # normal boundaries
+        start = i * base_size
+        end = (i + 1) * base_size if i < chunk_size - 1 else text_size
+
+        # apply overlap (except first chunk)
+        if i != 0:
+            start = max(0, start - overlap)
+
+        chunks.append(text[start:end])
+        print(f"chunk {i}: start={start}, end={end}")
+
+    return chunks
+
+
+text = "Machine learning and Artificial Intelligence is part of Data Science. The above skill is Gen AI in this field."
+vector_store = get_chunk(text, 3, 2)
+print(vector_store)
